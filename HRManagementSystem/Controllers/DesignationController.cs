473f1c88f4b85b0f00367941d0ffe3a6e28e5ecd
Darab Ahmed <darabahmed37@gmail.com>
2022-11-29 16:24:51 +0500

    Designation

---
+++ HRManagementSystem/Controllers/DesignationController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HRManagementSystem.Controllers {
+    public class DesignationController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
